60c0dd88b1745e218437ea8691e7fb91d08dd9b8
shoutaabe0530 <shoutaabe0530@gmail.com>
2026-08-05 17:29:17 +0900

    test: レビュールーチンの動作確認

---
+++ test-review.js
@@ -0,0 +1,8 @@
+﻿export function getNextWord(words, index) {
+  return words[index + 1];
+}
+
+export function loadProgress() {
+  const raw = localStorage.getItem("progress");
+  return JSON.parse(raw);
+}
